73f46a9a03fc35e77df5c7bbb524ae9c951d1c7d
James Koonts <jkoontsiii@gmail.com>
2022-11-09 17:00:06 -0500

    .

--- src/main/java/exifcomments/ExifApplication.java
+++ src/main/java/exifcomments/ExifApplication.java
@@ -4,14 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.lang.reflect.*;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class ExifApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("javafx-exif.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ExifApplication.class.getResource("javafx-exif.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
