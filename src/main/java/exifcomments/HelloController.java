cbc452174f077db1b21ef8bc6362d44d66be2e18
foxx <jkoontsiii@gmail.com>
2022-11-09 14:42:26 -0500

    .

--- src/main/java/exifcomments/HelloController.java
+++ src/main/java/exifcomments/HelloController.java
@@ -1,11 +1,15 @@
 package exifcomments;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Button clearButton;
+
 
     @FXML
     protected void onHelloButtonClick() {
