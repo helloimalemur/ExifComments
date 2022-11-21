ee37f36f5932944c81f6d3377ab60d859d27a51e
jkoonts <jkoontsiii@gmail.com>
2022-11-21 12:28:51 -0500

    .

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module exifcomment {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires org.apache.commons.imaging;
+
+//    opens net.koonts.exifcomments to javafx.fxml;
+
+    exports net.koonts.exifcomments;
+}
